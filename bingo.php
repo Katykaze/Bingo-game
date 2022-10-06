8a9a2e53c871a332715274f443e75ea88f1cc42e
katykaze <kathezambrano9@gmail.com>
2022-10-06 13:08:44 +0200

    bingo actual

--- bingo.php
+++ bingo.php
@@ -6,194 +6,94 @@
 
 <BODY>
     <?php
-    function crearCarton()
+
+
+    /*$jugador2 = array($carton1,$carton2,$carton4);
+    $jugador3 = array($carton1,$carton2,$carton4);
+    $jugador4 = array($carton1,$carton2,$carton4);*/
+    $carton1 = carton();
+    $carton2 = carton();
+    $carton3 = carton();
+    $jugador1 = array($carton1, $carton2, $carton3);
+
+
+    $carton = carton();
+    //imprimir($carton1);
+    //imprimir($carton2);
+    //imprimir($carton3);
+
+
+    function carton()
     {
         $carton = array();
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 7; $j++) {
-                $num = rand(1, 60);
-                $carton[$i][$j] = $num;
-            }
+        for ($i = 0; $i < 60; $i++) {
+            $carton[$i] = $i + 1;
         }
+        shuffle($carton);
+        //var_dump($carton);
+        $carton = array_slice($carton, 0, 15);
         return $carton;
     }
-    function eliminar($carton)
-    {
-        $pos = 0;
+    $carton = carton();
 
-        for ($i = 0; $i < count($carton); $i++) {
-            for ($j = 0; $j < count($carton[$i]); $j++) {
-                $pos2 = 0;
-                for ($x = 0; $x < 2; $x++) {
-                    $pos = rand(0, 6);
-                    if ($j == $pos && $pos != $pos2) {
-                        $carton[$i][$j] = 0;
-                    }
-                    $pos2 = $pos;
-                }
-            }
-        }
-        return $carton;
-    }
 
-    $carton = crearCarton();
-    $cartonBorrado = eliminar($carton);
-    imprimir($cartonBorrado);
-    //imprimir($carton);
     function imprimir($carton)
     {
         for ($i = 0; $i < count($carton); $i++) {
-            for ($j = 0; $j < count($carton[$i]); $j++) {
-                echo $carton[$i][$j];
-                echo " ";
-            }
-            echo "<br>";
+            echo $carton[$i] . " ";
         }
         printf("<br>");
         printf("<br>");
     }
     //funcion para tachar
-    function tacharCarton($carton)
+    imprimir($carton);
+    $bombo = bola();
+    echo count($bombo);
+    print_r($bombo);
+    tacharCarton($carton, $bombo);
+
+
+    function tacharCarton($carton, $bombo)
     {
-        $contador = 15;
-        do {
-            for ($i = 0; $i < count($carton); $i++) {
-                for ($j = 0; $j < count($carton[$i]); $j++) {
-                    //meter funcion de la bola aqui 
-                    $bola = 2;
-                    if ($bola == $carton[$i][$j]) {
-                        $carton[$i][$j] = 0;
-                        $contador--;
+        $contador = 0;
+        $cont = 0;
+            $numero = 0;
+           
+                for ($i = 0; count($bombo)-$cont; $i++) {
+                    $cont++;    
+                    $numero = $bombo[$i];
+                    for ($j = 0; $j < count($carton); $j++) {
+                        if ($numero == $carton[$j]) {
+                            $contador++;
+                            echo $numero . " ";
+                            echo "-----> contador $contador";
+                            echo "</br>";
+                        }
                     }
                 }
-            }
-        } while ($contador == 0);
-        return $carton;
-    }
-    $imprimirCarton = tacharCarton($cartonBorrado);
-    imprimir($imprimirCarton);
-    //------------------------funcion para tachar cartones y jugar
 
-    //------------------------jugadores --------------------------
-    function jugadores()
-    {
-        $jugadores = array(
-            'JugadorUno' => array(eliminar(crearCarton()), eliminar(crearCarton()),eliminar(crearCarton())),
-            'JugadorDos' => array(eliminar(crearCarton()), eliminar(crearCarton()), eliminar(crearCarton())),
-            'JugadorTres' => array(eliminar(crearCarton()), eliminar(crearCarton()), eliminar(crearCarton())),
-            'JugadorCuatro' => array(eliminar(crearCarton()), eliminar(crearCarton()), eliminar(crearCarton()))
-        );
-    }
-
-    function imprimirCartonesJugadores($jugadores)
-    {
-        foreach ($jugadores as $clave  => $valor) {
-            echo $clave;
-            for($i=0;$i<count($valor);$i++){
-                imprimir($i);
-
-
-            }
-        }
-    }
-
-    function jugar()
-    {
-        // jugadores();
-        // imprimir();
-        // bola();
-        // tacharCarton();
-
-        //------------------------------
-    }
-    
-    // FunciónBombo
-    
-     
-function bola(){
-     
-    $bombo=rand(1,60);
-   $fueraBombo=array();             
+            
            
-        if (!in_array($bombo,$fueradelBombo)) { 
-               array_push($fueradelBombo,$bombo);
-             shuffle($bombo);
+        
+
+
+        echo "Has ganado";
+    }
+    //funcion bola
+    function bola()
+    {
+        $bombo = array();
+        //$numero=0;
+        for ($i = 0; $i < 60; $i++) {
+
+            $bombo[$i] = $i + 1;
         }
-    return $bombo;
+        shuffle($bombo);
+        return $bombo;
+    }
 
-}
- 
 
-$carton=array();
-$i=0;  
-while($i<15)  
-{  
-    $bombo=rand(1,60);  
-  if ($bombo == 1) {
-    $num= '<img src="E:/images/1.PNG">';
-    echo $num;
-} elseif ($bombo == 2) {
-    $num= '<img src="E:/images/2.PNG">';
-    echo $num;
-    }elseif ($bombo == 3) {
-    $num= '<img src="E:/images/3.PNG">';
-    echo $num;
-} elseif ($bombo == 4) {
-    $num= '<img src="E:/images/4.PNG">';
-    echo $num;
-} elseif ($bombo == 5) {
-    $num= '<img src="E:/images/5.PNG">';
-    echo $num;
-    }elseif ($bombo == 6) {
-    $num= '<img src="E:/images/6.PNG">';
-    echo $num;
-} elseif ($bombo == 7) {
-    $num= '<img src="E:/images/7.PNG">';
-    echo $num;
 
-} elseif ($bombo == 8) {
-    $num= '<img src="E:/images/8.PNG">';
-    echo $num;
-    }elseif ($bombo == 9) {
-    $num= '<img src="E:/images/9.PNG">';
-    echo $num;
-} elseif ($bombo == 10) {
-    $num= '<img src="E:/images/10.PNG">';
-    echo $num;
-  
-  
-  
-  
-  
-  
-    if(in_array($bombo,$carton)===false) 
-    {  
-        array_push($carton,$bombo);  
-        $i++;  
-    }  
-}  
-
-//var_dump($carton);
-    echo "<br>";
-
-$numerosTachados=0;
-$fueradelBombo=array();
-
-    while($numerosTachados < count($carton)) { 
-        $numeroAleatorio=rand(1,60); 
-
-       if (!in_array($numeroAleatorio,$fueradelBombo)) { 
-            array_push($fueradelBombo,$numeroAleatorio);
-         
-        echo "número: $numeroAleatorio <br>";
-            //for($i=0; $i<count($carton); $i++){ 
-                //if($numeroAleatorio===$carton[$i]) { 
-                //$carton[$i]="X";  
-                //$numerosTachados++;
-                }
-            // echo("$carton[$i]/");
-            }
-             echo("<br>");
 
     ?>
 </BODY>
